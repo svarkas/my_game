14a514d7f23f5d703981fab3775fe8d7e7ad4dc4
svarkas <stvarkas@gmail.com>
2025-06-08 22:05:46 +0300

    trying to draw dice value

--- snake.py
+++ snake.py
@@ -1,12 +1,14 @@
 import pygame
 import sys
 import time
+import random
 
 pygame.init()
 size = width, height = 600, 400
 
 black = (0, 0, 0)
 white = (255, 255, 255)
+color = (50, 120, 60)
 
 screen = pygame.display.set_mode(size)
 
@@ -18,12 +20,36 @@ title = largeFont.render("Snake", True, white)
 titleRect = title.get_rect()
 titleRect.center = (width  // 2, 50)
 
-while True:
+running= True
+dice_value = ' '
+
+def draw_dice_value( dice, dice_value ) -> pygame.Rect:
+    dice_val = pygame.Rect(0, 0, 40, 40)
+    dice_val.center = dice.center
+    return dice_val
+
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
     screen.fill(black)
-
     screen.blit(title, titleRect)
-    pygame.display.flip() 
+
+    dice_button = pygame.Rect(width - 100, height - 100, 80 , 80)
+    dice = mediumFont.render(str(dice_value), True, black)
+    dice_rect = dice.get_rect()
+    dice_rect.center = dice_button.center
+    pygame.draw.rect(screen, white, dice_button)
+    screen.blit(dice, dice_rect)
+    
+    click, _, _ = pygame.mouse.get_pressed()
+    if click ==1:
+        mouse = pygame.mouse.get_pos()
+        if dice_button.collidepoint(mouse):
+            dice_value = random.randint(1, 6)
+            dice_val = draw_dice_value(dice_button, dice_value)
+            pygame.draw.rect(dice, color, dice_val)
+    pygame.display.flip()
+
+
